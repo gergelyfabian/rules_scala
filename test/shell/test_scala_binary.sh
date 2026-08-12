3837804d5ca8ed13f512a07f3da53a531af494d0
bor_p <36397564+bor-p-s@users.noreply.github.com>
2026-08-12 20:16:35 +0300

    refactor: Migrate scala_binary missing-direct-deps tests to Bazel (#1914)

--- test/shell/test_scala_binary.sh
+++ test/shell/test_scala_binary.sh
@@ -4,20 +4,6 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . "${dir}"/test_helper.sh
 runner=$(get_test_runner "${1:-local}")
 
-test_scala_binary_expect_failure_on_missing_direct_deps() {
-  dependency_target='//test_expect_failure/missing_direct_deps/internal_deps:transitive_dependency'
-  test_target='test_expect_failure/missing_direct_deps/internal_deps:user_binary'
-
-  test_scala_library_expect_failure_on_missing_direct_deps ${dependency_target} ${test_target}
-}
-
-test_scala_binary_expect_failure_on_missing_direct_deps_located_in_dependency_which_is_scala_binary() {
-  dependency_target='//test_expect_failure/missing_direct_deps/internal_deps:transitive_dependency'
-  test_target='test_expect_failure/missing_direct_deps/internal_deps:binary_user_of_binary'
-
-  test_scala_library_expect_failure_on_missing_direct_deps ${dependency_target} ${test_target}
-}
-
 test_scala_binary_allows_opt_in_to_use_of_argument_file_in_runner_for_improved_performance() {
 
   bazel run --extra_toolchains="//test/toolchains:use_argument_file_in_runner" //test/src/main/scala/scalarules/test/large_classpath:largeClasspath
@@ -36,9 +22,6 @@ test_scala_binary_allows_opt_in_to_use_of_argument_file_in_runner_for_improved_p
   fi
 
 }
-
-$runner test_scala_binary_expect_failure_on_missing_direct_deps
-$runner test_scala_binary_expect_failure_on_missing_direct_deps_located_in_dependency_which_is_scala_binary
 
 if !  is_windows; then
   #rules_scala doesn't support argfiles on windows yet
